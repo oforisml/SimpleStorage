00ff307bbb7f407d67df0d6298ac4f1b2a1bf199
oforisml <oforisml@gmail.com>
2025-05-05 15:42:58 +0000

    Simple Storage

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+contract SimpleStorage{
+    uint256 favoriteNumber;
+
+    People public person = People({favoriteNumber: 2, name: "Samuel O"});
+
+    People[] public peoples;
+
+
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
+
+    function addPeople (string memory _name, uint256 _favoriteNumber) public {
+        peoples.push(People(_favoriteNumber, _name));
+    }
+}
